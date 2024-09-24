958fdac4263a18e71e28c43ed1ee2114aae65b8b
LaamVu <lamvufc37@gmail.com>
2024-09-24 14:30:05 +0700

    Initial commit

---
+++ type_hint/class/A.py
@@ -0,0 +1,5 @@
+from class.C import C
+
+class A(C):
+    def a1(self):
+        print("Phương thức a1() trong class A")
---
+++ type_hint/class/C.py
@@ -0,0 +1,5 @@
+from interface.I import I
+
+class C(I):
+    def f(self):
+        print("Phương thức f() trong class C")
